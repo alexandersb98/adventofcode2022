542b4bcb558ec32581bec5de29cdb468c2fb2ed0
alexandersb98 <alexandersb98@gmail.com>
2022-12-03 13:47:55 +0100

    solved day 3 part 2

--- day3/solution.py
+++ day3/solution.py
@@ -1,6 +1,3 @@
-test_input_file = r'S:\Dokument\Programmering\Github\alexandersb98\adventofcode2022\day3\test_input.txt'
-input_file = r'S:\Dokument\Programmering\Github\alexandersb98\adventofcode2022\day3\input.txt'
-
 def get_input_lines(path: str):
     with open(path) as f:
         return f.readlines()
@@ -14,7 +11,7 @@ def get_priorities(items: str):
     return [get_priority(item) for item in items]
 
 def get_duplicates(c1: str, c2: str):
-    return {item for item in c1 if item in c2}
+    return {item for item in c1 if item in c2 and item != '\n'}
 
 def get_string_halves(s: str):
     if s == '':
@@ -25,13 +22,33 @@ def get_string_halves(s: str):
 def get_compartments(rucksack: str):
     return get_string_halves(rucksack)
     
+def get_groups(lines: list): 
+    group_size = 3
+    n_groups = int(len(lines) / group_size)
+    
+    groups = []
+    for i in range(n_groups):
+        a = group_size*i
+        b = a+group_size
+        group = lines[a:b]
+        groups.append(group)
+    return groups
+
+def get_badge(group):
+    a,b,c = group
+    duplicates = get_duplicates(a, get_duplicates(b, c))
+    badge = list(duplicates)[0]
+    return badge
+
+def get_badges(groups: list):
+    return [get_badge(group) for group in groups]
+
 
 #--------------------------------------------
-
-
-
-
 # PART 1
+
+test_input_file = r'S:\Dokument\Programmering\Github\alexandersb98\adventofcode2022\day3\test_input.txt'
+input_file = r'S:\Dokument\Programmering\Github\alexandersb98\adventofcode2022\day3\input.txt'
 
 rucksacks = get_input_lines(path=input_file)
 
@@ -43,3 +60,26 @@ for r in rucksacks:
     prio_sum += sum(priorities)
 
 print(f'Total sum = {prio_sum}')
+
+#--------------------------------------------
+# PART 2
+
+test_input_file = r'S:\Dokument\Programmering\Github\alexandersb98\adventofcode2022\day3\test_input2.txt'
+input_file = r'S:\Dokument\Programmering\Github\alexandersb98\adventofcode2022\day3\input2.txt'
+
+lines = get_input_lines(path=input_file)
+groups = get_groups(lines)
+
+group1 = groups[1]
+elf_a, elf_b, elf_c = group1
+badge = get_badge(group1)
+print(badge)
+
+badges = get_badges(groups)
+
+print(badges)
+
+priorities = get_priorities(badges)
+total_prio = sum(priorities)
+print(f'Total priority of all badges = {total_prio}')
+    
