c8bc6cb44a8efc3f14ad0988d2d7eceb89493ef0
alexandersb98 <alexandersb98@gmail.com>
2022-12-04 22:57:31 +0100

    solved day 4 part 2

--- day4/solution.py
+++ day4/solution.py
@@ -26,5 +26,15 @@ def any_covers(a, b):
 def covers(a, b):
     return a[0] <= b[0] and a[1] >= b[1]
 
+def overlaps(a: tuple, b: tuple):
+    left_is_contained = a[0] in range(b[0], b[1]+1)
+    right_is_contained = a[1] in range(b[0], b[1]+1)
+    return left_is_contained or right_is_contained or any_covers(a,b)
+
+# Part 1
 fully_covering_pairs = [pair for pair in pairs if any_covers(pair[0], pair[1])]
 print(f'Number of pairs in which one range fully covers the other = {len(fully_covering_pairs)}')
+
+# Part 2
+overlapping_pairs = [p for p in pairs if overlaps(p[0], p[1])]
+print(f'Number of pairs that overlap each other = {len(overlapping_pairs)}')
